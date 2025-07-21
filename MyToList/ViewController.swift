5e64adf072e06efc417b60df9a853c92c69b8a45
verysh <vladerysh@gmail.com>
2025-07-21 17:39:54 +0300

    Initial Commit

---
+++ MyToList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyToList
+//
+//  Created by Vladimir Eryshev on 21.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
